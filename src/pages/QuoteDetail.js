1ed48bf1978c6edd49cc8bad5bd12db2c7e20b15
Ramses <rlarabel@gmail.com>
2021-10-14 15:50:43 -0400

    Initial Commit

---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
+import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetail = () => {
+    const match = useRouteMatch();
+    const params = useParams();
+
+    //Use Obj descructure so useEffect can be percise
+    const { quoteId } = params
+
+    const { sendRequest, status, data: loadedQuote, error} = useHttp(getSingleQuote, true);
+
+    useEffect(() => {
+        sendRequest(quoteId);
+    }, [sendRequest, quoteId]);
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        );
+    }
+
+    if (error) {
+        return <p className='centered'>{error}</p>;
+    }
+
+    if (!loadedQuote.text) {
+        return <p>No Quote Found!</p>
+    }
+
+    return (
+        <section>
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+            <Route path={match.path} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>View Comments</Link>
+                </div>
+            </Route>
+            <Route path={`${match.path}/comments`}>
+                <Comments/>
+            </Route>
+        </section>
+    );
+};
+
+export default QuoteDetail;
